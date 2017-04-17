4aeb6c3b74ddcc66b7853ce28b7fd1bdfb0a31ee
Lynn Cherny <arnicas@gmail.com>
2017-04-17 11:57:09 +0200

    added extra quotes for searches

--- collect_tweets.py
+++ collect_tweets.py
@@ -21,11 +21,11 @@ auth.set_access_token(cred.ACCESS_TOKEN, cred.ACCESS_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 
-SEARCHES = ["@SAFRAN", "@Alstom","@airliquidegroup","@TechnipGroup",
-            "@SolvayGroup","@Rexel_Group","@VolvoTrucksFR","@orexad_FR",
-            "@Capgemini","@PublicisGroupe","@ENGIEgroup","@ArcelorMittal",
-            "@Intel","@Cisco","@Forrester","@Adobe","@Salesforce",
-            "@Oracle","@MaerskLine","@Generalelectric","@VMware"]
+SEARCHES = ['"@SAFRAN"', '"@Alstom"','"@airliquidegroup"','"@TechnipGroup"',
+            '"@SolvayGroup"','"@Rexel_Group"','"@VolvoTrucksFR"','"@orexad_FR"',
+            '"@Capgemini"','"@PublicisGroupe"','"@ENGIEgroup"','"@ArcelorMittal"',
+            '"@Intel"','"@Cisco"','"@Forrester"','"@Adobe"','"@Salesforce"',
+            '"@Oracle"','"@MaerskLine"','"@Generalelectric"','"@VMware"']
 
 #SEARCHES = ['@Adobe', '@Cisco']
 
